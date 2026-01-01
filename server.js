2ae0ff8deb341b2c69471d3063938b11894cfc36
Creator@AAV <64518918+ommehta4920@users.noreply.github.com>
2026-01-01 12:40:35 +0530

    index file path updated

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname)));
 
 // ✅ Explicit root fallback (important)
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // Start server
