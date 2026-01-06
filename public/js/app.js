b569c312578c06da31195a12ee77652fc8c9bea3
Creator@AAV <64518918+ommehta4920@users.noreply.github.com>
2026-01-06 17:13:32 +0530

    Campaign ROAS Removed

--- public/js/app.js
+++ public/js/app.js
@@ -2439,7 +2439,6 @@ function renderCampaignWiseTable(campaignData) {
             <td class="text-right">${clicks.toLocaleString()}</td>
             <td class="text-right"><strong>${leads}</strong></td>
             <td class="text-right">₹${avgCPL.toFixed(2)}</td>
-            <td class="text-right">${avgROAS > 0 ? avgROAS.toFixed(2) + 'x' : 'N/A'}</td>
             <td class="text-right">${ctr.toFixed(2)}%</td>
             <td class="text-right">${convRate.toFixed(2)}%</td>
             <td class="text-center">
